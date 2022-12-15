38a128696b850fe92c816b294c60e98e0c38c2fc
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-12-15 23:48:52 +0100

    update

--- js/apicall.js
+++ js/apicall.js
@@ -1,11 +1,11 @@
-const url = `https://pokeapi.co/api/v2/pokemon/`;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
 
 async function pokemonApi() {
   const response = await fetch(url);
   console.log(response);
   if (!response.ok) {
-    const message =
-
+    const message = `An error has occurred: ${response.status}`;
+    throw new Error(message);
   }
   const json = await response.json();
   console.log(json);
