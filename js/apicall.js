b5b3e954954f196f85cf756dcd264bf45876e76f
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-12-15 23:39:35 +0100

    update

--- js/apicall.js
+++ js/apicall.js
@@ -3,6 +3,10 @@ const url = `https://pokeapi.co/api/v2/pokemon/`;
 async function pokemonApi() {
   const response = await fetch(url);
   console.log(response);
+  if (!response.ok) {
+    const message =
+
+  }
   const json = await response.json();
   console.log(json);
 }
