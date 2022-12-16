eba0c07b842dc1d14a8a30b03352a39b82396bfd
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-12-16 01:25:10 +0100

    update

--- js/apicall.js
+++ js/apicall.js
@@ -1,5 +1,7 @@
-const url = `https://pokeapi.co/api/v2/pokemon?limit=150&offset=0`;
-
+// const url = `https://pokeapi.co/api/v2/pokemon?limit=150&offset=0`;
+const url = `https://pokeapi.co/api/v2/pokemon/`;
+const cards = document.querySelector(".cards");
+// const url = `https://pokeapi.co/api/v2/pokemon/ditto`;
 async function pokemonApi() {
   try {
     const response = await fetch(url);
@@ -10,6 +12,14 @@ async function pokemonApi() {
     }
     const json = await response.json();
     console.log(json);
+    const property = json.results;
+    console.log(property);
+
+    property.forEach((element) => {
+      cards.innerHTML += `<p>${element.name}<p/>
+                          <p>${element.url}<p/>
+                            `;
+    });
   } catch (error) {
     console.log("Fetch error: ", error);
   }
