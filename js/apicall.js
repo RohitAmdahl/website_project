53ff37d5de1ed0da2fdc9de613e3fe868ac19fef
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-01-05 13:16:45 +0100

    update

--- js/apicall.js
+++ js/apicall.js
@@ -1,13 +1,16 @@
 const url = `https://pokeapi.co/api/v2/pokemon/?limit=150`;
 const cards = document.querySelector(".cards");
+
 async function pokemonApi() {
   try {
     const response = await fetch(url);
     console.log(response);
+
     if (!response.ok) {
       const message = `An error has occurred: ${response.status}`;
       throw new Error(message);
     }
+
     const json = await response.json();
     console.log(json);
     const property = json.results;
@@ -15,9 +18,11 @@ async function pokemonApi() {
 
     property.forEach((element) => {
       const urlArr = element.url.split("/");
+      console.log(urlArr);
       const id = (urlArr.pop(), urlArr.pop());
+      console.log(id);
       cards.innerHTML += `<h1 class="name-poke">${element.name}</h1>
-                           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="" class = "pokeimg"/> `;
+                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="" class = "pokeimg"/> `;
     });
   } catch (error) {
     console.log("Fetch error: ", error);
