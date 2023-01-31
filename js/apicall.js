5e5c93839a39d300095e9fed0a197112cc3a0d9c
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-01-31 13:38:40 +0100

    testing with tailwind

--- js/apicall.js
+++ js/apicall.js
@@ -21,8 +21,8 @@ async function pokemonApi() {
       console.log(urlArr);
       const id = (urlArr.pop(), urlArr.pop());
       console.log(id);
-      cards.innerHTML += `<h1 class="name-poke text-sky-900">${element.name}</h1>
-                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="" class = "pokeimg"/> `;
+      cards.innerHTML += `<h1 class="name-poke text-sky-900 block text-lg">${element.name}</h1>
+                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="" class="object-contain h-48 w-96"/> `;
     });
   } catch (error) {
     console.log("Fetch error: ", error);
